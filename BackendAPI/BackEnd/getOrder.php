2068c45ae34b39e9446c1ba3a04d4f0d00ef24b8
wiradaniramadhan <adhan432@gmail.com>
2017-06-01 12:19:13 +0700

    new api again

--- BackendAPI/BackEnd/getOrder.php
+++ BackendAPI/BackEnd/getOrder.php
@@ -6,7 +6,8 @@
 
   include 'config.php';
     $id=$_GET['user'];
-    $query_user = mysqli_query($conn, "SELECT * FROM order_goods s join user WHERE user_id='$id'");
+    $query_user = mysqli_query($conn, "SELECT * FROM order_goods s JOIN user r JOIN addres k
+      ON s.user_id=r.user_id AND k.user_id=r.user_id AND s.shop_id='$id'");
 
     $result_set = array();
     while($result =mysqli_fetch_assoc($query_user)){
