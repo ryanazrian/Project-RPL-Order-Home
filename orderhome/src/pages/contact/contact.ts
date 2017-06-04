d25801f14488a4ea53a20dc0943be9fc96ce509f
wiradaniramadhan <adhan432@gmail.com>
2017-06-04 20:36:13 +0700

    new update

--- orderhome/src/pages/contact/contact.ts
+++ orderhome/src/pages/contact/contact.ts
@@ -20,7 +20,7 @@ export class ContactPage {
    }
 
    getdataPembeli(){
-    this.http.get("http://127.0.0.1/OrderHome/BackEnd/getOrder.php?user=15").subscribe(data => {
+    this.http.get("http://127.0.0.1/OrderHome/BackEnd/getOrder.php?user=1").subscribe(data => {
       let response = data.json();
       console.log(response);
       if(response.status=="200"){
@@ -29,60 +29,6 @@ export class ContactPage {
     });
   }
 
-  // getPenjual() {
-  //   this.userData.getUsername().then((username) => {
-  //     this.user_id = user_id;
-  //   });
-  // }
-   selesai(data){
-    // let id_patient = data.id_patient;
-    let confirm = this.alertCtrl.create({
-      title: 'Anda yakin?',
-      // message: 'B pasien tidak bisa dibatalkan',
-      buttons: [
-        {
-          text: 'Tidak',
-          handler: () => {
-            console.log('Disagree clicked');
-          }
-        },
-        {
-          text: 'Ya',
-          handler: () => {
-            // console.log('Agree clicked');
-
-            // this.http.get(this.data.BASE_URL+"/delete_patient.php?patient="+id_patient).subscribe(data => {
-            //     let response = data.json();
-            //     console.log(response);
-            //     if(response.status=="200"){
-            //       this.pasien= response.data;
-            //       let alert = this.alertCtrl.create({
-            //         title: 'Pasien Terhapus',
-            //         subTitle: '',
-            //         buttons: ['OK']
-            //       });
-            //       this.vibration.vibrate(1000);
-            //       alert.present();
-
-            //       this.ionViewWillEnter();
-            //     }
-            //     else {
-            //           let alert = this.alertCtrl.create({
-            //         title: 'Gagal Menghapus',
-            //         subTitle: '',
-            //         buttons: ['OK']
-            //       });
-            //       this.vibration.vibrate(1000);
-            //       alert.present();
-            //     }
-            //   });
-            }
-
-          }
-
-      ]
-    });
-    confirm.present();
-  }
+  
 
 }
